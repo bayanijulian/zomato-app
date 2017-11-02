7549455045ed4f4ab7323f9d2e21140ee9680a0e
Bayani Julian <bayanijulian@gmail.com>
2017-11-01 21:15:52 -0500

    implemented ViewHolder for RestaurantAdapter

--- app/src/main/java/edu/illinois/zomatoapp/RestaurantAdapter.java
+++ app/src/main/java/edu/illinois/zomatoapp/RestaurantAdapter.java
@@ -3,12 +3,13 @@ package edu.illinois.zomatoapp;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import edu.illinois.zomatoapp.api.restaurant.RestaurantHolder;
+import edu.illinois.zomatoapp.api.restaurant.Restaurant;
 
 /**
  * Created by bayanijulian on 11/1/17.
@@ -16,9 +17,9 @@ import edu.illinois.zomatoapp.api.restaurant.RestaurantHolder;
 
 public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ViewHolder> {
 
-    private List<RestaurantHolder> restaurants;
+    private List<Restaurant> restaurants;
 
-    public RestaurantAdapter(RestaurantHolder[] restaurants){
+    public RestaurantAdapter(Restaurant[] restaurants) {
         this.restaurants = new ArrayList<>();
         this.restaurants.addAll(Arrays.asList(restaurants));
     }
@@ -38,9 +39,20 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Vi
         return restaurants.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public View itemView;
+        public TextView nameTextView;
+        public TextView cuisineTextView;
+        public TextView priceRangeTextView;
+        public TextView addressTextView;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            this.itemView = itemView;
+            this.nameTextView = (TextView) itemView.findViewById(R.id.restaurantNameTextView);
+            this.cuisineTextView = (TextView) itemView.findViewById(R.id.restaurantCuisineTextView);
+            this.priceRangeTextView = (TextView) itemView.findViewById(R.id.restaurantPriceRangeTextView);
+            this.addressTextView = (TextView) itemView.findViewById(R.id.restaurantAddressTextView);
         }
     }
 }
