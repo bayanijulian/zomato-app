ea7f93412052a70f40ca75b2e37a4ab4bbba0064
Bayani Julian <bayanijulian@gmail.com>
2017-11-07 15:08:00 -0600

    RestaurantDetailActivity shows all data with buttons
    The RestaurantDetailActivity uses the restaurant extra from the intent
    to display the name, cuisines, location, price range, and average price.
    Two buttons were added to launch their website and a map for directions.

--- app/src/main/java/edu/illinois/zomatoapp/RestaurantDetailActivity.java
+++ app/src/main/java/edu/illinois/zomatoapp/RestaurantDetailActivity.java
@@ -1,9 +1,12 @@
 package edu.illinois.zomatoapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import edu.illinois.zomatoapp.api.restaurant.Restaurant;
@@ -20,9 +23,50 @@ public class RestaurantDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurant_detail);
 
         Intent intent = getIntent();
-        Restaurant restaurant = intent.getParcelableExtra(RestaurantAdapter.EXTRA_RESTAURANT);
-        final TextView name = (TextView) findViewById(R.id.restaurantDetailNameTextView);
-        name.setText(restaurant.getName());
+        final Restaurant restaurant = intent.getParcelableExtra(RestaurantAdapter.EXTRA_RESTAURANT);
+        final TextView nameTxt = (TextView) findViewById(R.id.restaurantDetailNameTextView);
+        final TextView cuisineTxt = (TextView) findViewById(R.id.restaurantCuisineTextView);
+        final TextView priceRangeTxt = (TextView) findViewById(R.id.restaurantPriceRangeTextView);
+        final TextView averagePriceTxt = (TextView) findViewById(R.id.restaurantDetailAveragePriceTextView);
+        final TextView locationTxt = (TextView) findViewById(R.id.restaurantDetailLocationTextView);
 
+        nameTxt.setText(restaurant.getName());
+        cuisineTxt.setText(restaurant.getCuisines());
+        priceRangeTxt.setText(getPriceRangeString(restaurant.getPriceRange()));
+        averagePriceTxt.setText(restaurant.getAverageCostForTwo());
+        locationTxt.setText(restaurant.getLocation().getAddress());
+
+        final Button websiteBtn = (Button) findViewById(R.id.restaurantDetailWebsiteBtn);
+        final Button mapBtn = (Button) findViewById(R.id.restaurantDetailMapBtn);
+
+        websiteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent1 = new Intent(Intent.A)
+            }
+        });
+
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Uri locationUri = Uri.parse("geo:0,0?q=" + restaurant.getLocation().getLatitude() +
+                                                "," + restaurant.getLocation().getLongitude());
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, locationUri);
+                if(mapIntent.resolveActivity(getPackageManager())!=null){
+                    startActivity(mapIntent);
+                }
+            }
+        });
+    }
+
+    private String getPriceRangeString(int priceRange) {
+        StringBuilder priceRangeSymbols = new StringBuilder();
+
+        for (int i = 0; i < priceRange; i++) {
+            priceRangeSymbols.append("$");
+        }
+
+        return priceRangeSymbols.toString();
     }
 }
