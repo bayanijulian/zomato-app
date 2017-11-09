4e7302c0806e84f01c77c148336d268d36671b2d
Bayani Julian <bayanijulian@gmail.com>
2017-11-08 21:45:46 -0600

    refactored RestaurantDetailActivity

--- app/src/main/java/edu/illinois/zomatoapp/RestaurantDetailActivity.java
+++ app/src/main/java/edu/illinois/zomatoapp/RestaurantDetailActivity.java
@@ -11,11 +11,16 @@ import android.widget.TextView;
 
 import edu.illinois.zomatoapp.api.restaurant.Restaurant;
 
-/**
- * Created by bayanijulian on 11/7/17.
- */
 
 public class RestaurantDetailActivity extends AppCompatActivity {
+    private TextView nameTxt;
+    private TextView cuisineTxt;
+    private TextView priceRangeTxt;
+    private TextView averagePriceTxt;
+    private TextView locationTxt;
+
+    private Button websiteBtn;
+    private Button mapBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,31 +29,55 @@ public class RestaurantDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         final Restaurant restaurant = intent.getParcelableExtra(RestaurantAdapter.EXTRA_RESTAURANT);
-        final TextView nameTxt = (TextView) findViewById(R.id.restaurantDetailNameTextView);
-        final TextView cuisineTxt = (TextView) findViewById(R.id.restaurantDetailCuisinesTextView);
-        final TextView priceRangeTxt = (TextView) findViewById(R.id.restaurantDetailPriceRangeTextView);
-        final TextView averagePriceTxt = (TextView) findViewById(R.id.restaurantDetailAveragePriceTextView);
-        final TextView locationTxt = (TextView) findViewById(R.id.restaurantDetailLocationTextView);
 
+        createTextViews();
+        bindDataToTextViews(restaurant);
 
-        String averagePrice = ""  + restaurant.getAverageCostForTwo();
+        createButtons();
+        createButtonListeners(restaurant);
+    }
+
+    private void createButtons() {
+        websiteBtn = (Button) findViewById(R.id.restaurantDetailWebsiteBtn);
+        mapBtn = (Button) findViewById(R.id.restaurantDetailMapBtn);
+    }
+
+    private void createTextViews() {
+        nameTxt = (TextView) findViewById(R.id.restaurantDetailNameTextView);
+        cuisineTxt = (TextView) findViewById(R.id.restaurantDetailCuisinesTextView);
+        priceRangeTxt = (TextView) findViewById(R.id.restaurantDetailPriceRangeTextView);
+        averagePriceTxt = (TextView) findViewById(R.id.restaurantDetailAveragePriceTextView);
+        locationTxt = (TextView) findViewById(R.id.restaurantDetailLocationTextView);
+    }
+
+    private void bindDataToTextViews(Restaurant restaurant) {
+        String averagePrice = "" + restaurant.getAverageCostForTwo();
 
         nameTxt.setText(restaurant.getName());
         cuisineTxt.setText(restaurant.getCuisines());
         priceRangeTxt.setText(getPriceRangeString(restaurant.getPriceRange()));
         averagePriceTxt.setText(averagePrice);
         locationTxt.setText(restaurant.getLocation().getAddress());
+    }
 
-        final Button websiteBtn = (Button) findViewById(R.id.restaurantDetailWebsiteBtn);
-        final Button mapBtn = (Button) findViewById(R.id.restaurantDetailMapBtn);
+    private String getPriceRangeString(int priceRange) {
+        StringBuilder priceRangeSymbols = new StringBuilder();
 
+        for (int i = 0; i < priceRange; i++) {
+            priceRangeSymbols.append("$");
+        }
+
+        return priceRangeSymbols.toString();
+    }
+
+    private void createButtonListeners(final Restaurant restaurant) {
         websiteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Uri websiteUri = Uri.parse(restaurant.getUrl());
                 Intent webIntent = new Intent(Intent.ACTION_VIEW, websiteUri);
 
-                if(webIntent.resolveActivity(getPackageManager())!=null){
+                if (webIntent.resolveActivity(getPackageManager()) != null) {
                     startActivity(webIntent);
                 }
             }
@@ -61,20 +90,10 @@ public class RestaurantDetailActivity extends AppCompatActivity {
                 Uri locationUri = Uri.parse("geo:0,0?q=" + restaurantNameAsUri);
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, locationUri);
 
-                if(mapIntent.resolveActivity(getPackageManager())!=null){
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
                     startActivity(mapIntent);
                 }
             }
         });
-    }
-
-    private String getPriceRangeString(int priceRange) {
-        StringBuilder priceRangeSymbols = new StringBuilder();
-
-        for (int i = 0; i < priceRange; i++) {
-            priceRangeSymbols.append("$");
-        }
-
-        return priceRangeSymbols.toString();
     }
 }
